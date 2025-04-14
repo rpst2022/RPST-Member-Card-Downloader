dbde96f1a917dee457abf4d81d819674399f911b
rpst2022 <rpst.rajapur@gmail.com>
2025-04-14 16:34:11 +0600

    Add files via upload

---
+++ members.js
@@ -0,0 +1,21 @@
+const members = [
+    {
+      id: "1082914",
+      name: "Md Oliur Rahman",
+      phone: "01912641577",
+      email: "oliur292007@gmail.com"
+    },
+    {
+      id: "1000002",
+      name: "Ayesha Khatun",
+      phone: "9123456780",
+      email: "ayesha@example.com"
+    },
+    {
+      id: "1000003",
+      name: "Binod Basnet",
+      phone: "9801234567",
+      email: "binod@example.com"
+    }
+  ];
+  
